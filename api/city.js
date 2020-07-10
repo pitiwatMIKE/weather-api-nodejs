b94252e61548ee68fee9a85b04c7776b4bbb2f04
pitiwatMIKE <pitiwat.pr61@gmail.com>
2020-07-10 16:55:39 +0700

    edit city.js banglol-> bangkok

--- api/city.js
+++ api/city.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const settings = require("../settings.js");
-const city = (cityName = "Banglol",countryCode="th")=>{
+const city = (cityName = "Bangkok",countryCode="th")=>{
     return axios
         .get(`${settings.apiUrl}?q=${cityName},${countryCode}&appid=${settings.apiKey}&units=metric`);
 };
